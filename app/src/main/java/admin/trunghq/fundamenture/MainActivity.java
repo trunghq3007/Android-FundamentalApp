01e74a247490882f00442c6ff37cef707ff15b94
ha quang trung <haquangtrungceo@gmail.com>
2018-06-01 09:05:39 +0700

    change

--- app/src/main/java/admin/trunghq/fundamenture/MainActivity.java
+++ app/src/main/java/admin/trunghq/fundamenture/MainActivity.java
@@ -8,6 +8,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.IOException;
+
 /**
  * The type Main activity.
  */
@@ -16,7 +18,9 @@ public class MainActivity extends Activity {
     /**
      * The Button open other activity.
      */
-    Button buttonOpenOtherActivity;
+    private Button buttonOpenOtherActivity;
+    private Button btnOpenThreeActivity;
+    private SinhVien sinhVien;
     // logt
     private static final String TAG = "MainActivity";
 
@@ -32,15 +36,44 @@ public class MainActivity extends Activity {
         Log.d("log", "MainActivity chay");
         Log.d(TAG, "onCreate: ");
         buttonOpenOtherActivity = findViewById(R.id.btnOpenOtherActivity);
+        btnOpenThreeActivity = findViewById(R.id.btnOpenThreeActivity);
+
+
         buttonOpenOtherActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d("log", "MainActivity onClick");
                 //Log.d("log","onClickOpenOtherActivity");
                 // Open OtherActivity Screen
+                // khai báo và khởi tạo explicit intent
                 Intent intent = new Intent(MainActivity.this, OtherActivity.class);
                 // open activity
+                intent.putExtra("key_data", "dữ liệu truyền sang từ MainActivity");
+                sinhVien = new SinhVien("Truong Gia Binh",58);
+                intent.putExtra("objSinhVien",sinhVien);
+                Bundle bundle = new Bundle();
+                bundle.putString("key_String_data","string in bundle");
+                bundle.putInt("key_Int_data",1);
+                //bundle.putin
+
+             /*   int x = 10;
+                float f = 1;
+                double d = 5;
+
+                Integer X = 10;
+                Float F = 100f;
+                Long L = 1000L;*/
+
                 startActivity(intent);
+                //startActivity(intent);
+            }
+        });
+        btnOpenThreeActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent();
+                intent.setAction("actionOpenThreeActivity");
                 startActivity(intent);
             }
         });
@@ -50,12 +83,24 @@ public class MainActivity extends Activity {
 
     }
 
-    /**
-     * Show.
-     */
-    public  void show(){
 
+    /**
+     * Caculate int.
+     *
+     * @param x the x
+     * @param y the y
+     * @param z the z
+     * @param f the f
+     *
+     * @return the int
+     *
+     * @throws IOException               the io exception
+     * @throws IndexOutOfBoundsException the index out of bounds exception
+     */
+    public static int caculate(int x, int y, double z, float f) throws IndexOutOfBoundsException {
+        return 0;
     }
+
     @Override
     protected void onRestart() {
         super.onRestart();
--- app/src/main/java/admin/trunghq/fundamenture/OtherActivity.java
+++ app/src/main/java/admin/trunghq/fundamenture/OtherActivity.java
@@ -1,10 +1,12 @@
 package admin.trunghq.fundamenture;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class OtherActivity extends AppCompatActivity {
     private static final String TAG = "OtherActivity";
@@ -32,6 +34,12 @@ public class OtherActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        Intent intentFromMainActivity = getIntent();
+        String dataFromMainActivity = intentFromMainActivity.getStringExtra("key_data");
+        Toast.makeText(this, dataFromMainActivity, Toast.LENGTH_SHORT).show();
+        SinhVien objSinhVien = (SinhVien) intentFromMainActivity.getParcelableExtra("objSinhVien");
+        //int i = MainActivity.caculate(1, 3, 4, 5);
+        Toast.makeText(this, objSinhVien.toString(), Toast.LENGTH_SHORT).show();
         Log.d("log", "onResume: OtherActivity");
     }
 
@@ -62,7 +70,7 @@ public class OtherActivity extends AppCompatActivity {
      *
      * @return the int
      */
-    public int  show(int x, int j){
-        return x+j;
+    public int show(int x, int j) {
+        return x + j;
     }
 }
